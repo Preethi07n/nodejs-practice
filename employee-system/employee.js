fb15c0407280177e0c3df3b3ebb87be6bafb1bf8
Preethi07n <preethi0720@gmail.com>
2026-03-31 09:07:16 +0530

    code push

---
+++ employee-system/employee.js
@@ -0,0 +1,48 @@
+import  readlineSync  from 'readline-sync';
+
+export async function addEmployee(emp) {
+    const res = await fetch("http://localhost:3000/employees", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(emp)
+    });
+    const data = await res.json();
+    console.log("Data Added", data);
+}
+
+
+export async function getEmployee() {
+    const res = await fetch("http://localhost:3000/employees");
+    const data = await res.json();
+    console.log("View Data", data);
+}
+
+export async function updateEmployee(id, updatedData) {
+    const res = await fetch(`http://localhost:3000/employees/${id}`, {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(updatedData)
+    });
+    const data = await res.json();
+    console.log("Data Added", data);
+}
+
+export async function deleteEmployee(id) {
+    const res = await fetch(`http://localhost:3000/employees/${id}`, {
+        method: "DELETE"
+    });
+    if(res.ok) {
+        console.log("Deleted Successfully");
+    }
+}
+
+export async function searchEmployee(query) {
+    const res = await fetch("http://localhost:3000/employees");
+    const data = await res.json();
+    const result = data.filter(emp => emp.id === query || emp.name === query || emp.salary === query);
+    console.log("Search result", result);
+}
---
+++ employee-system/index.js
@@ -0,0 +1,41 @@
+import { addEmployee, getEmployee, updateEmployee, deleteEmployee, searchEmployee } from './employee.js';
+import  readlineSync  from 'readline-sync';
+
+export async function main() {
+   
+const enterOperation  = readlineSync.questionInt("Enter Operation; 1. Add, 2. View, 3: Update, 4: Delete, 5: Search \n");
+switch(enterOperation) {
+    case 1:
+       await addEmployee({
+        id: readlineSync.questionInt("id:", ),
+        name: readlineSync.question("name:", ),
+        email: readlineSync.question("email:", ),
+        salary: readlineSync.questionInt("salary", ),
+       })
+       break;
+    case 2:
+        await getEmployee();
+        break;
+    case 3:
+        const id =  readlineSync.questionInt("id:", );
+        await updateEmployee(id, {
+              id: readlineSync.questionInt("id:", ),
+        name: readlineSync.question("name:", ),
+        email: readlineSync.question("email:", ),
+        salary: readlineSync.questionInt("salary", ),
+        })
+        break;
+    case "4":
+        const idDelete =  readlineSync.questionInt("id:", );
+        await deleteEmployee(idDelete);
+        break;
+    case 5:
+        const search =  readlineSync.questionInt("id:", );
+        await searchEmployee(search);
+        break;
+    default:
+    console.log('invalid');
+}
+}
+
+main();
